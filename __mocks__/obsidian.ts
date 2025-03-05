100cc78e842b42bd44d3379d33aa607ed707cdd5
Mike Rodarte <github@mts7.com>
2025-03-05 14:02:38 -0500

    add third test suite for Setting Tab

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -19,7 +19,36 @@ module.exports = {
     WorkspaceLeaf: jest.fn(),
     Editor: jest.fn(),
     PluginSettingTab: jest.fn(),
-    Setting: jest.fn(),
     ItemView: jest.fn(),
     CustomEvent: jest.fn(),
+    Setting: jest.fn().mockImplementation((containerEl: HTMLElement) => {
+        const settingEl = document.createElement('div');
+        const textArea = document.createElement('textarea');
+        textArea.classList.add('word-frequency-setting-blacklist');
+        settingEl.appendChild(textArea);
+        containerEl.appendChild(settingEl);
+
+        return {
+            setName: jest.fn().mockReturnThis(),
+            setDesc: jest.fn().mockReturnThis(),
+            addTextArea: jest.fn().mockImplementation((callback) => {
+                callback({
+                    setValue: function (value: string) {
+                        textArea.value = value;
+                        return this;
+                    },
+                    onChange: function (handler: (value: string) => void) {
+                        textArea.addEventListener('change', (event) => {
+                            handler((event.target as HTMLTextAreaElement).value);
+                        });
+                        return this;
+                    },
+                    inputEl: textArea,
+                });
+                return {
+                    inputEl: textArea,
+                };
+            }),
+        };
+    }),
 };
--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,18 @@ module.exports = {
     testEnvironment: 'jsdom',
     modulePathIgnorePatterns: ['<rootDir>/dist/'],
     testMatch: ['<rootDir>/src/__tests__/**/*.test.ts'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['cobertura', 'html', 'text', 'text-summary'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.d.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 };
